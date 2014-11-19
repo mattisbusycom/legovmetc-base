b050cdcda935a3b0934760afe0f05b655332eb01
root <root@ip-172-31-22-173.us-west-2.compute.internal>
2014-11-19 16:51:55 +0000

    pictures

--- set.php
+++ set.php
@@ -14,22 +14,29 @@ $pley_cdn_url = $data['cdnUrl'];
 foreach ($pley['content'] as $key => $set)
 {
     $data['set_id']     = $set['sku'];
-    $data['theme']       = $set['theme'];
+    $data['theme']      = $set['theme'];
 
-    $data['Set']['name']  = $set['title'];
+    $data['Set']['name']        = $set['title'];
+    $data['Set']['price']       = $set['price'];
     $data['Set']['description'] = $set['description'];
 
-
-    $data['Set']['price'] = $set['price'];
     $data['Set']['age_start'] = $set['ageStart'];
     $data['Set']['age_end']   = $set['ageEnd'];
     $data['Set']['gender']    = $set['gender'];
 
     $images = $set['imageList']; 
-    if (array_key_exists('at1x', $images)) {
-    	foreach ($images['at1x'] as $picture) {
-	print_r($picture);	
-   	 }
+    if (array_key_exists('at1x', $images['main'])) {
+    	foreach ($images['main']['at1x'] as $picture) {
+		print "PATH: ".$picture."\n";
+		//$data['Set']['image_urls'][] = $picture['path'];
+   	}
     }
-    print_r($data);
+
+    $stores = $set['storeList'];
+    if (array_key_exists('0', $stores)) {
+	$data['Set']['LegoStore'][] = array('retailer' => $stores[0]['name'], 'link' => $stores[0]['url']);
+    }
+    if (array_key_exists('1', $stores)) {
+	$data['Set']['LegoStore'][] = array('retailer' => $stores[1]['name'], 'link' => $stores[1]['url']);
+    }
 }
